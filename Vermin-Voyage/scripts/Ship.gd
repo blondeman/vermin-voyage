d07b695150f830f44b93d8109a4126336f641f7a
blondeman <domo23000@gmail.com>
2024-09-29 22:45:37 -0600

    created ship object and build function
    removed isMiddle from shipnode and added reference to opposite node, where null means middle

--- Vermin-Voyage/scripts/Ship.gd
+++ Vermin-Voyage/scripts/Ship.gd
@@ -7,28 +7,43 @@ var shipLineScene = preload("res://scenes/ShipLine.tscn")
 var nodePositions: Array = [Vector2(50,100), Vector2(450,30), Vector2(850,100)]
 var nodeLines: Array = [Vector2(0,1), Vector2(1,2)]
 
-var symmetryLine: float = 0 
+var symmetryLine: float = 100 
 var nodes: Array = []
 var lines: Array = []
 
 func _ready():
-	build()
+	build(nodePositions, nodeLines)
 	
-func build():
+func build(nodePositions: Array, nodeLines: Array):
 	for i in range(nodePositions.size()):
 		var node = shipNodeScene.instantiate()
-		add_child(node)
-		
-		if i == 0 or i == nodePositions.size() - 1:
-			node.isMiddle = true
 		node.position = nodePositions[i]
+		
+		add_child(node)
 		nodes.append(node)
-	
+		
+		if i != 0 and i != nodePositions.size() - 1:
+			var oppositeNode = shipNodeScene.instantiate()
+			oppositeNode.position = node.oppositePosition(symmetryLine)
+			node.opposite = oppositeNode
+			oppositeNode.opposite = node
+			
+			add_child(oppositeNode)
+
+
 	for i in range(nodeLines.size()):
 		var line = shipLineScene.instantiate()
-		add_child(line)
-		
 		line.nodeA = nodes[nodeLines[i].x]
 		line.nodeB = nodes[nodeLines[i].y]
 		line.setLine()
+		
+		add_child(line)
 		lines.append(line)
+		
+		var oppositeLine = shipLineScene.instantiate()
+		oppositeLine.nodeA = nodes[nodeLines[i].x].getOpposite()
+		oppositeLine.nodeB = nodes[nodeLines[i].y].getOpposite()
+		oppositeLine.setLine()
+		
+		add_child(oppositeLine)
+		lines.append(oppositeLine)
--- Vermin-Voyage/scripts/ShipLine.gd
+++ Vermin-Voyage/scripts/ShipLine.gd
@@ -1,8 +1,8 @@
 extends Line2D
 class_name ShipLine
 
-@export var nodeA: ShipNode = null
-@export var nodeB: ShipNode = null
+var nodeA: ShipNode = null
+var nodeB: ShipNode = null
 
 func setLine():
 	points = [nodeA.position, nodeB.position]
